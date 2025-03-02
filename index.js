5306c91f5dbf629bdcd545f9fc3622f2d8eda465
mdtspramanik <189224128+mdtspramanik@users.noreply.github.com>
2025-03-02 17:16:54 +0600

    feat(homepage): create homepage layout

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+// Serve static files on the specified port
+app.use(express.static("public"));
+
+// Render index.ejs file
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+// Start the server on the specified port
+app.listen(port, () => {
+  console.log(`Backend server is running on port http://localhost:${port}`);
+});
