3b98212e33595953f4003f8c733a3d31a1c76dff
mdtspramanik <189224128+mdtspramanik@users.noreply.github.com>
2025-03-04 15:49:40 +0600

    fix(find page): update find page

--- index.js
+++ index.js
@@ -6,14 +6,24 @@ const port = 3000;
 // Serve static files on the specified port
 app.use(express.static("public"));
 
+// Middleware for parsing URL-encoded data
+app.use(express.urlencoded({ extended: true }));
+
 // Render index.ejs file
 app.get("/", (req, res) => {
   res.render("index.ejs");
 });
 
 // Render add.ejs file
-app.get("/add", (req, res) => {
-  res.render("add.ejs");
+app.get("/find-page", (req, res) => {
+  res.render("find-page.ejs");
+});
+
+app.post("/find-book", (req, res) => {
+  const isbn = req.body.search;
+  const imageUrl = `https://covers.openlibrary.org/b/isbn/${isbn}-L.jpg?default=false`;
+
+  res.render("find-page.ejs", { imageUrl, isbn });
 });
 
 // Start the server on the specified port
